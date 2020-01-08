06aa61dd8a019e1a63a3fa14a4064ea44bf57f8b
NukesedukE <58881582+NukesedukE@users.noreply.github.com>
2020-01-08 07:44:14 +0100

    Create uninstallString.ps1

---
+++ uninstallString.ps1
@@ -0,0 +1,9 @@
+#PROD SCRIPT to get uninstall strings
+
+$fooAppname = read-host 'Application Name'
+$foo32bit = Get-Itemproperty 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, DisplayVersion, UninstallString | Where-Object { $_.DisplayName -match "^*$fooAppname*"}
+$foo64bit = Get-Itemproperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, DisplayVersion,  UninstallString | Where-Object { $_.DisplayName -match "^*$fooAppname*"}
+write-host "32Bit:"
+$foo32bit | ft
+write-host "64Bit:"
+$foo64bit | ft
